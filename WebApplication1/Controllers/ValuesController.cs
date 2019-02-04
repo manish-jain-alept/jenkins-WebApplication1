2d4a4cda64d438f270740977f6d3f1e887862130
manish-jain-alept <manish.jain@alept.com>
2019-02-04 10:34:33 +0530

    1.PasswordSalt & Hash 2.CustomProfile modified

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -12,7 +12,61 @@ namespace WebApplication1.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            // **********
+            // PASSWORD TO HASH
+            // **********
+            string myPassword = "Alept1#";
+            string savedPasswordHash = PasswordToHash(myPassword);
+            // **********
+            // HASH TO PASSWORD
+            // **********
+            bool Validate = HashToPassword(savedPasswordHash, myPassword + "1");
+
+            return new string[] { myPassword, "value2" };
+        }
+
+        public string PasswordToHash(string myPassword)
+        {
+            // SALT + ORIGINAL_PASSWORD = HASH
+
+            // GENERATE RANDOM SALT
+            byte[] salt;
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            // ITERATION OF 10000, MAKE THIS FUNCTION SLOW. SO ATTACKER CAN'T FREQUENTLY TRY NEW PASSWORD
+            var pbkdf2 = new System.Security.Cryptography.Rfc2898DeriveBytes(myPassword, salt, 10000);
+
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public bool HashToPassword(string savedPasswordHash, string myPassword)
+        {
+            bool validate = false;
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            byte[] salt = new byte[16];
+
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            var pbkdf2 = new System.Security.Cryptography.Rfc2898DeriveBytes(myPassword, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            //PASSWORD COMPARISION
+            int ok = 1;
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                    ok = 0;
+
+                if (ok == 1)
+                {
+                    validate = true;
+                }
+            }
+            return validate;
         }
 
         // GET api/values/5
